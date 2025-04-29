31001afe05b5ff6b9318e3faad159f7fab7c28b2
liamzebedee <liamzebedee@yahoo.com.au>
2025-04-29 20:08:21 +1000

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Model, useController, AsyncResult } from "@liamzebedee/stateman.js";
 import { AppController } from "./controller";
 
-let appController = new AppController();
+const appController = new AppController();
 
 
 
